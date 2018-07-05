bcda4f88c530f6a4b327f1fcff041220f2b96ed5
Eric Ellison <er.ellison.nyc@gmail.com>
2018-07-05 21:08:30 +0430

    adding JSX loader to webpack (commented)

--- webpack.config.js
+++ webpack.config.js
@@ -48,6 +48,17 @@ module.exports = {
           }
         ]
       }
+      // loaders: [
+      //   {
+      //     test: /\.jsx?$/,
+      //     exclude: /(node_modules|bower_components)/,
+      //     loader: 'babel-loader',
+      //     query: {
+      //       presets: ['react', 'es2015', 'stage-0'],
+      //       plugins: ['react-html-attrs', 'transform-decorators-legacy', 'transform-class-properties'],
+      //     }
+      //   }
+      // ]
     ]
   },
 
