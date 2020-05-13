e9d16ec5b2454c6f4a43f29aafd87054ccd833a4
a_m_dev <a.mirzaei.dev@gmail.com>
2020-05-13 16:59:31 +0430

    last update getting result 99 percent

--- src/js/Components/Datepicker/RangePicker/RangePickerManager.js
+++ src/js/Components/Datepicker/RangePicker/RangePickerManager.js
@@ -389,16 +389,39 @@ const RangePickerManager = (props) => {
     Object.entries(oldState).forEach(([date, state]) => {
       const isExsists = dates.some((dt) => dt === date);
 
+      console.log("-->>", date);
+
       if (isExsists && type === "SINGLE") {
         state.isIncluded = !state.isIncluded;
+        if (state.isInSequence && state.isIncluded) {
+          state.forceIncluded = true;
+          console.log(1.1);
+        }
         Object.assign(x, { [date]: state });
+        console.log(1);
+      } else if (!isExsists && type === "BULK" && state.isInSequence) {
+        state.isIncluded = true;
+        state.isInSequence = false;
+        Object.assign(x, { [date]: state });
+        console.log(2);
       } else if (isExsists) {
         state.isIncluded = false;
         if (type === "BULK") {
           state.isInSequence = true;
+          if (state.forceIncluded) {
+            state.isIncluded = true;
+            state.forceIncluded = true;
+            console.log(2.2);
+          } else if (!state.forceIncluded) {
+            state.isIncluded = false;
+            state.isInSequence = true;
+            console.log(2.3);
+          }
         }
         Object.assign(x, { [date]: state });
+        console.log(3);
       }
+      console.log("----------------------------------");
     });
 
     console.log("XXXX", { x });
